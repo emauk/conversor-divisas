5e7abe55fb14df3324a4f7c7c07acd8ddb1e32d2
Emmanueluk <ema.skillet.kn@gmail.com>
2025-05-15 03:43:07 -0600

    Menú con formato creado

--- com/conversor/emmanuel/principal/Principal.java
+++ com/conversor/emmanuel/principal/Principal.java
@@ -2,16 +2,41 @@ package com.conversor.emmanuel.principal;
 
 import com.conversor.emmanuel.conversiones.ConversorDivisas;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
-
+/*
         double saldo=0;
         double cantidad = 33;
-
         ConversorDivisas cambio = new ConversorDivisas();
         saldo = cambio.Conversion(cantidad);
+        DecimalFormat df = new DecimalFormat("#.##");
+        System.out.println(cantidad + " dolares equivalen a: "+df.format(saldo)+" MXN");*/
 
-        System.out.println(cantidad + " dolares equivalen a: "+saldo);
+       /* Scanner entrada = new Scanner(System.in);
+        int opcion= entrada.nextInt();
+*/
+        //MENU
+        System.out.println("-- Bienvenido al programa de conversión de divisas --");
+        System.out.println("#################################\n# 1) Dólar => Peso argentino    #\n# " +
+                "2) Peso argentino => Dólar    #\n# 3) Dólar => Real Brasileño    #\n# 4) Real brasileño " +
+                "=> Dólar    #\n# 5) Dólar => Peso colombiano   #\n# 6) Peso colombiano => Dólar   #\n# 7) " +
+                "Salir                      # \n# Elige una opción              #" +
+                "\n#################################");
+        //switch
+      /*  switch (opcion){
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+
+
+        }*/
 
     }
 }
